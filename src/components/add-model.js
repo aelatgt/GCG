6a2b277874617c2099b103cb079dfe46fbab02d7
Angela Dai <52553063+angles-d@users.noreply.github.com>
2021-03-26 21:07:22 -0400

    add add-morph-object component

--- src/components/add-model.js
+++
@@ -1,8 +0,0 @@
-import '../components/gtlf-morph.js'
-var entered="none"
-AFRAME.registerComponent('add-model', {
-    dependencies: ['raycaster="objects: [data-raycastable]"'],
-    init: function () {
-        
-    }
-});
--- src/components/gtlf-morph.js
+++ src/components/gtlf-morph.js
@@ -1,12 +1,10 @@
+//Source: https://github.com/elbobo/aframe-gltf-morph-component
 AFRAME.registerComponent('gltf-morph', {
-
   multiple: true,
-
   schema:{
     morphtarget: {type: 'string', default: ''},
     value: {type: 'number', default: 0},
   },//end schema
-
   init: function(){
 
     //make sure object3D is set at first
@@ -16,37 +14,24 @@ AFRAME.registerComponent('gltf-morph', {
       this.morpher()
 
     });
-
   },//end init
-  
   update: function(){
-    
     //run morpher function
     this.morpher()
-    
   },//end update
 
   morpher: function(){
-    
     //get the mesh
     var mesh = this.el.object3D
-    
     //traverse it looking for morphtargets and targetnames
     mesh.traverse((o) => {
-
       //if morphtargets exist & targetNames exist
       if ( o.morphTargetInfluences && o.userData.targetNames ){
-
         //store the array position of the passed targetname
         var pos = o.userData.targetNames.indexOf(this.data.morphtarget);
-
         //target that morphtarget and update value
         o.morphTargetInfluences[pos] = this.data.value
-
       }//end if
-
     });//end traverse
-    
   },//end morpher
-
 })//end morph
